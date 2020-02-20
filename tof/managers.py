63b69a48f6391dac86a096c0bbc0e74ac39208eb
pilik <pilikinp@yandex.ru>
2020-02-20 08:46:44 +0300

    order_by

--- tof/managers.py
+++ tof/managers.py
@@ -4,6 +4,9 @@
 # @Last Modified by:   MaxST
 # @Last Modified time: 2019-11-19 16:40:49
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+
+from django.utils.translation import get_language
 
 from .decorators import tof_filter, tof_prefetch
 
@@ -20,6 +23,25 @@ class DecoratedMixIn:
     @tof_filter  # noqa
     def get(self, *args, **kwargs):
         return super().get(*args, **kwargs)
+
+    def order_by(self, *args, **kwargs):
+        from .models import Translation, TranslatableField
+        content_type_id = ContentType.objects.get_for_model(self.model).id
+        new_args = []
+        subqueries = {}
+        for arg in args:
+            field_name, rev = arg, ''
+            if arg.startswith('-'):
+                field_name, rev = arg[1:], arg[0]
+            if isinstance(getattr(self.model, field_name), TranslatableField):
+                subquery = Translation.objects.filter(object_id=models.OuterRef('pk'), lang=get_language(),
+                                                      field=f'{field_name}|{content_type_id}').values('value')
+                new_args.append(f'{rev}_{field_name}')
+                subqueries[f'_{field_name}'] = subquery
+            else:
+                new_args.append(arg)
+        self = self.model.objects.annotate(**subqueries)
+        return super().order_by(*new_args, **kwargs)
 
 
 class TranslationsQuerySet(DecoratedMixIn, models.QuerySet):
